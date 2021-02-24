0ec41f1103ab13c0bc870d8a95feaff5c0f2014c
Trevor Loula <trevorloula@outlook.com>
2021-02-24 15:19:58 -0500

    Line Detection Updated

--- lines/line_detection.py
+++ lines/line_detection.py
@@ -11,6 +11,9 @@ sys.path.insert(1, '/home/autonav/autonav_ws/src/autonav_utils')
 
 import cv2
 import numpy as np
+import rospy
+
+from performance_analyzer import PerformanceAnalyzerLite
 
 class LineDetection:
 
@@ -20,49 +23,66 @@ class LineDetection:
         self.aligned = False
         self.found_line = False
 
-    def set_params(self, is_debug, BUFF_SIZE, BUFF_FILL, CROP_BOTTOM, CROP_SIDE):
+        self.pa = PerformanceAnalyzerLite("general_purpose_pa")
+
+    def set_params(self, is_debug, BUFF_SIZE, BUFF_FILL, CROP_TOP, CROP_BOTTOM, CROP_SIDE, MAX_WHITE, MIN_LINE_LENGTH, trevor_debug):
         self.is_debug = is_debug
         self.BUFF_SIZE = BUFF_SIZE
         self.BUFF_FILL = BUFF_FILL
+        self.CROP_TOP = CROP_TOP
         self.CROP_BOTTOM = CROP_BOTTOM
         self.CROP_SIDE = CROP_SIDE
+        self.MAX_WHITE = MAX_WHITE
+        self.MIN_LINE_LENGTH = MIN_LINE_LENGTH
         self.line_history = [0] * self.BUFF_SIZE
+        self.TREVOR_DEBUG = trevor_debug
 
     def image_callback(self, image):
+        self.pa.start()
         # Save Dimensions
         y, x = image.shape[0], image.shape[1]
 
         # Slice Edges
-        image = image[:y-int(y*self.CROP_BOTTOM), int(x*self.CROP_SIDE):x-int(x*self.CROP_SIDE)]
+        image = image[int(y*self.CROP_TOP):-int(y*self.CROP_BOTTOM), int(x*self.CROP_SIDE):-int(x*self.CROP_SIDE)]
 
         # Convert to Greyscale and Threshold
         ret, grey =  cv2.threshold(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        # Open Image
-        morph = cv2.morphologyEx(grey, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15)))
+        # Static threshold
+        #ret, grey = cv2.threshold(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), 190, 255, cv2.THRESH_BINARY)
+
+        # Discard Oversaturated Images
+        if np.count_nonzero(grey) < image.shape[0]*image.shape[1]*self.MAX_WHITE:
+            # Open Image
+            morph = cv2.morphologyEx(grey, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)))
+            self.determine_line(morph)
+            if self.TREVOR_DEBUG:
+                cv2.imshow("Line Detection Opened Image", morph)
+                cv2.waitKey(1)
+        else:
+            rospy.loginfo("Line Detection Image Discarded")
+            self.update_history(0)
 
         # Display Image
-        if self.is_debug:
-            cv2.imshow("Thresholded Image", grey)
-            cv2.waitKey(1)
-            cv2.imshow("Opened Image", morph)
+        if self.TREVOR_DEBUG:
+            #cv2.imshow("Line Detection Cropped Image", image)
+            #cv2.waitKey(1)
+            cv2.imshow("Line Detection Thresholded Image", grey)
             cv2.waitKey(1)
 
-        self.determine_line(morph)
         found_updated = self.determine_state()
         aligned_updated = self.determine_orientation()
 
+        self.pa.stop()
         return found_updated, self.found_line, aligned_updated, self.aligned
 
     def determine_line(self, image):
-        lines = cv2.HoughLinesP(image, 1, np.pi/180, 150, minLineLength=250, maxLineGap=40)
+        lines = cv2.HoughLinesP(image, 1, np.pi/180, 150, minLineLength=int(image.shape[0]*self.MIN_LINE_LENGTH), maxLineGap=40)
         if lines is not None:
             x1, y1, x2, y2 = lines[0][0]
             self.slope = float(abs(y2-y1)) / float(abs(x2-x1)+0.00001)
-            self.line_history[self.history_idx] = 1
-        else:
-            self.line_history[self.history_idx] = 0
-        self.history_idx = (self.history_idx + 1) % self.BUFF_SIZE
+            self.update_history(1)
+        else: self.update_history(0)
 
     def determine_state(self):
         if not self.found_line and self.line_history.count(1) >= self.BUFF_FILL * self.BUFF_SIZE:
@@ -81,3 +101,12 @@ class LineDetection:
             self.aligned = False
             return True
         return False
+
+    def update_history(self, x):
+        self.line_history[self.history_idx] = x
+        self.history_idx = (self.history_idx + 1) % self.BUFF_SIZE
+
+    def reset(self):
+        self.line_history = [0] * self.BUFF_SIZE
+        self.found_line = False
+        self.aligned = False
--- lines/lines.py
+++ lines/lines.py
@@ -10,11 +10,11 @@ import sys
 sys.path.insert(1, '/home/autonav/autonav_ws/src/autonav_utils')
 
 import rospy
+from autonav_node import AutoNavNode
 from line_detection import LineDetection
 from line_following import LineFollowing
 from sensor_msgs.msg import Image, CameraInfo
 from std_msgs.msg import String
-from autonav_node import AutoNavNode
 
 class Lines(AutoNavNode):
 
@@ -30,8 +30,12 @@ class Lines(AutoNavNode):
         # Read ROS Params - Line Detection
         self.read_param("/LineDetectBufferSize", "DETECT_BUFF_SIZE", 10)
         self.read_param("/LineDetectBufferFill", "DETECT_BUFF_FILL", 0.8)
-        self.read_param("/LineDetectCropBottom", "CROP_BOTTOM", 0.52)
+        self.read_param("/LineDetectCropTop", "CROP_TOP", 0.00)
+        self.read_param("/LineDetectCropBottom", "CROP_BOTTOM", 0.20)
         self.read_param("/LineDetectCropSide", "CROP_SIDE", 0.20)
+        self.read_param("/LineDetectMaxWhite", "MAX_WHITE", .50)
+        self.read_param("/LineDetectMinLineLength", "MIN_LINE_LENGTH", 0.35)
+        self.read_param("/TrevorDebug", "TREVOR_DEBUG", False)
 
         # Read ROS Params - Line Following
         self.read_param("/IsaiahDebug", "ISAIAH_DEBUG", False)
@@ -53,16 +57,27 @@ class Lines(AutoNavNode):
         # Publish distance from the center of the line
         self.motor_pub = rospy.Publisher("wheel_distance", String, queue_size=10)
 
+        # Subscribe to state updates for the robot
+        self.state_sub = rospy.Subscriber("state_topic", String, self.state_callback)
+
         # Initialize Classes
         self.line_detection = LineDetection()
         self.line_following = LineFollowing()
+
+        # Line Detection States Set
+        self.line_detection_states = {self.OBJECT_AVOIDANCE_FROM_LINE, self.OBJECT_TO_LINE, self.FIND_LINE, self.LINE_ORIENT}
 
         # Set ROS Params
         self.line_detection.set_params(
             self.is_debug,
             self.DETECT_BUFF_SIZE,
+            self.DETECT_BUFF_FILL,
+            self.CROP_TOP,
             self.CROP_BOTTOM,
-            self.CROP_SIDE)
+            self.CROP_SIDE,
+            self.MAX_WHITE,
+            self.MIN_LINE_LENGTH,
+            self.TREVOR_DEBUG)
         self.line_following.set_params(
             self.following_dir,
             self.is_debug,
@@ -79,28 +94,33 @@ class Lines(AutoNavNode):
     def image_callback(self, ros_image):
         image = self.bridge_image(ros_image, "bgr8")
 
-        # Line Following    
-        if self.state == self.CHANGE_TO_LINE_FOLLOWING:
+        #rospy.logwarn("LINES STATE: {}".format(self.state))
+
+        # Line Followings
+        if self.state == self.LINE_FOLLOWING:
+            self.line_detection.reset()
+            rospy.logwarn("L")
             distance = self.line_following.image_callback(image)
             rospy.loginfo("LINE_DISTANCE: " + str(distance))
-            self.motor_pub.publish(self.LINE_CODE + str(distance))
+            self.safe_publish(self.motor_pub, self.LINE_CODE + str(distance))
 
         # Line Detection
-        elif self.state == self.CHANGE_TO_OBJECT_AVOIDENCE_FROM_LINE:
+        elif self.state in self.line_detection_states:
             found_updated, found_line, aligned_updated, aligned = self.line_detection.image_callback(image)
+            rospy.logwarn("Line Detection")
             if found_updated and found_line:
-                rospy.loginfo(self.FOUND_LINE)
-                self.event_pub.publish(self.FOUND_LINE)
+                rospy.logwarn(self.FOUND_LINE)
+                self.safe_publish(self.event_pub, self.FOUND_LINE)
             elif found_updated:
-                rospy.loginfo(self.LOST_LINE)
-                self.event_pub.publish(self.LOST_LINE)
+                rospy.logwarn(self.LOST_LINE)
+                self.safe_publish(self.event_pub, self.LOST_LINE)
 
             if aligned_updated and aligned:
-                rospy.loginfo(self.ALIGNED)
-                self.event_pub.publish(self.ALIGNED)
+                rospy.logwarn(self.ALIGNED)
+                self.safe_publish(self.event_pub, self.ALIGNED)
             elif aligned_updated:
-                rospy.loginfo(self.NOT_ALIGNED)
-                self.event_pub.publish(self.NOT_ALIGNED)
+                rospy.logwarn(self.NOT_ALIGNED)
+                self.safe_publish(self.event_pub, self.NOT_ALIGNED)
 
 def main(args):
     lines = Lines()
@@ -108,8 +128,6 @@ def main(args):
     try: rospy.spin()
     except KeyboardInterrupt:
         rospy.loginfo("Keyboard interrupt")
-    finally:
-        rospy.loginfo("Shutting down line class")
 
 if __name__ == "__main__":
     main(sys.argv)
